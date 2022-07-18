45793cfbeec984597496a1992289e81f30925570
abhishekgarfield <abhishek2759@gmail.com>
2022-07-18 17:58:24 +0530

    git clone

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function Car(props) {
+    return <li>I am a { props.brand }</li>;
+}
+
+function Garage() {
+    const cars = [
+        {id: 1, brand: 'Ford'},
+        {id: 2, brand: 'BMW'},
+        {id: 3, brand: 'Audi'}
+    ];
+    return (
+        <>
+            <h1>Who lives in my garage?</h1>
+            <ul>
+                {cars.map((item) => <Car key={item.id} brand={item.brand} />)}
+            </ul>
+        </>
+    );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<Garage />);
